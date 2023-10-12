3255101a54fecb0b3b7aeeff0ee1e7d1c09bb271
Lee Merrow <leemerrow@gmail.com>
2023-10-12 18:55:36 -0400

    made sql code shorter

--- sql/fashion_magazines.sql
+++ sql/fashion_magazines.sql
@@ -1,17 +1,10 @@
 -- Add your sql here
-WITH unpaid AS(
-	SELECT o.customer_id,
-		c.customer_name AS Customer,
-		o.order_status,
-		s.price_per_month * s.subscription_length AS Amount
-	FROM orders o
-	JOIN customers c
-		ON c.customer_id = o.customer_id 
-	JOIN subscriptions s 
-		ON s.subscription_id = o.subscription_id 
-	WHERE o.order_status = "unpaid" AND s.description = 'Fashion Magazine'
-)
-SELECT unpaid.Customer,
-	PRINTF('$%.2f',SUM(unpaid.Amount)) AS 'Amount Due'
-FROM unpaid
-GROUP BY Customer;
+SELECT c.customer_name AS Customer,
+	PRINTF('$%.2f', SUM(s.price_per_month * s.subscription_length)) AS 'Amount Due'
+FROM orders o
+JOIN customers c
+	ON c.customer_id = o.customer_id 
+JOIN subscriptions s 
+	ON s.subscription_id = o.subscription_id 
+WHERE o.order_status = "unpaid" AND s.description = 'Fashion Magazine'
+GROUP BY c.customer_name;
